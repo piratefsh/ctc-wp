cede45ed798d50e03c1bca78e7c5b8acff1105cb
Sher Minn Chong <sherminn.chong@gmail.com>
2015-06-11 10:13:51 +0800

    Style page

--- wp-content/themes/ctc/page.php
+++ wp-content/themes/ctc/page.php
@@ -1,14 +1,16 @@
 <?php get_header(); ?>
 
 <?php get_template_part('partials/navigation'); ?>
-<div class="uk-container uk-container-center uk-grid ">
-    <article class="uk-width-4-5">
-		<h1> <?php the_title(); ?> </h1>
-        <?php if($post->post_content) :?>
-            <?php while ( have_posts() ) : the_post();?>
-                <?php the_content(); ?>
-            <?php endwhile; ?>
-        <?php endif;?>
+<div class="uk-container uk-container-center">
+    <article class="uk-article">
+		<h1 class="uk-article-title"> <?php the_title(); ?> </h1>
+        <div class="uk-grid">
+	        <?php if($post->post_content) :?>
+	            <?php while ( have_posts() ) : the_post();?>
+	                <?php the_content(); ?>
+	            <?php endwhile; ?>
+	        <?php endif;?>
+        </div>
     </article>
 </div>
 
